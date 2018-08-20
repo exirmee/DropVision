3a8aea10771cc9f9693f86efe7388313c1125d4c
exirius <m.hatami@live.com>
2018-08-20 13:50:50 +0330

    29.5

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ class App:
         self.root.title(window_title)
         self.video_source = video_source
         self.analysis_switch=False
+        self.show_switch=False
         self.csvList=[]
         #read configuration file
         self.config = ConfigObj('conf.cnf')
@@ -72,35 +73,40 @@ class App:
         self.referenceText=tk.Spinbox(self.root,width=8,textvariable=self.referenceVar, from_=0, to=1000)
         self.referenceText.pack(in_=self.right)
         self.referenceVar.set("1")
-
+        
+        # creating show stream button 
+        self.showButton = Button(self.root, text="ShowStream",command=self.showStram,width=18, padx="2", pady="3")
+        self.showButton.pack(in_=self.subtop1)
+       
+        # creating start show expriment button 
+        self.satrtButton = Button(self.root,  text="Start Analysis",width=18, padx="2", pady="3",command=self.threadCap)
+        self.satrtButton.pack(in_=self.subtop1)
+        
         # strat calibrating proccess (set left and right niddle coornidates)
         self.calibrateButton = Button(self.root,  text="Calibration",width=18, padx="2", pady="3",command=self.showCalibrate)
-        self.calibrateButton.pack(in_=self.subtop1)
+        self.calibrateButton.pack(in_=self.subtop2)
 
         # show setting form
         self.settingButton = Button(self.root,  text="Setting",width=18, padx="2", pady="3",command=self.showSetting)
-        self.settingButton.pack(in_=self.subtop1)
+        self.settingButton.pack(in_=self.subtop2)
 
-        # creating start show expriment button 
-        self.satrtButton = Button(self.root,  text="Start Analysis",width=18, padx="2", pady="3",command=self.threadCap)
-        self.satrtButton.pack(in_=self.subtop2)
+
 
         # creating start record expriment button to excel
         self.recButton = Button(root,  text="Export CSV",width=18, padx="2", pady="3",command=self.startRec)
-        self.recButton.pack(in_=self.subtop2)
+        self.recButton.pack(in_=self.subtop3)
         # 33333333333333333333333333333333
         self.chartButton = Button(root,  text="Show Chart",width=18, padx="2", pady="3",command=self.show_chart)
         self.chartButton.pack(in_=self.subtop3)
+        
         # creating start record expriment button to excel
         self.captureButton = Button(self.root,  text="Capture Frame",width=18, padx="2", pady="3",command=self.snapshot)
-        self.captureButton.pack(in_=self.subtop3)
+        self.captureButton.pack(in_=self.subtop4)
 
         # creating reset button 
         self.quitButton = Button(self.root, text="Reset",command=self.client_reset,width=18, padx="2", pady="3")
         self.quitButton.pack(in_=self.subtop4)
-        # creating exit button 
-        self.quitButton = Button(self.root, text="Exit",command=self.client_reset,width=18, padx="2", pady="3")
-        self.quitButton.pack(in_=self.subtop4)
+
 
 
         # tree view to show temp analisys data
@@ -152,11 +158,26 @@ class App:
             self.analysis_switch=False
         sett.fire()
 
+    #show stream job and change button text    
+    def showStram(self):
+        if self.show_switch:
+            
+            self.showButton["text"]="ShowStream"
+            
+            self.show_switch=False
+        else:
+            self.showButton["text"]="StopStram"
+            
+            self.show_switch=True
+            
+            self.root.after(self.delay, self.updateShow)
+
     #start analysis job and change button text    
     def threadCap(self):
         if self.analysis_switch:
             self.satrtButton["text"]="Start Analysis"
             self.analysis_switch=False
+            
         else:
             self.satrtButton["text"]="Stop Analysis"
             self.analysis_switch=True
@@ -168,28 +189,26 @@ class App:
     def startRec(self):
         print(self.csvList)
         f =  (filedialog.asksaveasfilename(initialdir = "/",title = "Select file",filetypes = (("csv files","*.csv"),("all files","*.*"))))+'.csv'
-       
-        
-        with open(self.experimentVar.get()+'.csv', 'w', newline='') as csvfile:
+        with open('.csv', 'w', newline='') as csvfile:
             spamwriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
             spamwriter.writerows(self.csvList)
-
-        copyfile(self.experimentVar.get()+'.csv', f)
+        copyfile('.csv', f)
 
     #reset everythong in form
     def client_reset(self):
         self.satrtButton["text"]="Start Analysis"
         self.analysis_switch=False
-        self.config["p1x"]="0"
-        self.config["p2x"]="0"
-        self.experimentVar.set("myExpriment")
         self.tree.delete(*self.tree.get_children())
         self.csvList.clear()
     
     #capture a frame into jpeg file
     def snapshot(self):
+        #read configuration file and put it to config array
+        config = ConfigObj('conf.cnf')
         # Get a frame from the video source
-        ret, frame = self.vid.get_frame()
+        vid=cv2.VideoCapture(int(config["path"]))
+        # Get a frame from the video source
+        ret, frame = vid.read()
         f =  filedialog.asksaveasfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
     
         if ret:
@@ -199,10 +218,11 @@ class App:
     def update(self):
         #read configuration file and put it to config array
         config = ConfigObj('conf.cnf')
-        self.delay = self.config["interval"]
+        self.delay = config["interval"]
         # Get a frame from the video source
-        ret, frame = self.vid.get_frame()
-        
+        vid=cv2.VideoCapture(int(config["path"]))
+        ret, frame = vid.read()
+        frame = cv2.resize(frame, (530, 397)) 
         #read frame from video and convert to gray then thresh then find edge
         if config["Ds"]=="top":
             frame = cv2.flip(frame, 1)
@@ -228,69 +248,76 @@ class App:
         cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE,
         cv2.CHAIN_APPROX_NONE)
         cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-        c = max(cnts, key=cv2.contourArea)
-        c=cnts[0]
-        #cv2.drawContours(frame, [c], 0, (0,255,0), 1)
-        # if the contour is not sufficiently large, ignore it
-        cv2.drawContours(frame,c, 0, (0, 255, 0), 2)
+        if cnts:
+            print('ok!')
+            c = max(cnts, key=cv2.contourArea)
+            c=cnts[0]
+            #cv2.drawContours(frame, [c], 0, (0,255,0), 1)
+            # if the contour is not sufficiently large, ignore it
+            cv2.drawContours(frame,c, 0, (0, 255, 0), 2)
 
-        # determine the most extreme points along the contour
-        deRight = tuple(c[c[:, :, 0].argmax()][0])
-        deLeftTemp = tuple(c[c[:, :, 0].argmin()][0])
-        deLeft = (deLeftTemp[0],deRight[1])
-        deMid=int(round((deRight[0]+deLeft[0])/2))
-        deDownTemp = tuple(c[c[:, :, 1].argmax()][0])
-        deDown= (deMid,deDownTemp[1])
-        deTopTemp=abs(abs(deDown[1])-abs(deLeft[0]-deRight[0]))
-        deTop= (deDown[0],deTopTemp)
+            # determine the most extreme points along the contour
+            deRight = tuple(c[c[:, :, 0].argmax()][0])
+            deLeftTemp = tuple(c[c[:, :, 0].argmin()][0])
+            deLeft = (deLeftTemp[0],deRight[1])
+            deMid=int(round((deRight[0]+deLeft[0])/2))
+            deDownTemp = tuple(c[c[:, :, 1].argmax()][0])
+            deDown= (deMid,deDownTemp[1])
+            deTopTemp=abs(abs(deDown[1])-abs(deLeft[0]-deRight[0]))
+            deTop= (deDown[0],deTopTemp)
+            # dsTemp is the coornidates of countour that is same as deTop
+            dsTemp=c[np.where(c[:,:,:]==deTop[1]), 0 ][0]
         
-        #define ds and de
-        DsMetric=0.0
-        DeMetric=0.0
+            #define ds and de
+            DsMetric=0.0
+            DeMetric=0.0
+
+            # if dsTemp has coornidates then grab most left and most right and put it to dsLeft,dsRight
+            if dsTemp.size:
+                dsLeft=(np.amax(dsTemp[:,0]),deTop[1])
+                dsRight=(np.amin(dsTemp[:,0]),deTop[1])
+                # put circles and coordinates of dsLeft and dsRight in screen
+                cv2.circle(frame, dsLeft, 5, (0, 255, 12), -1)
+                cv2.circle(frame, dsRight, 5, (0, 255, 12), -1)
+                #calculate distance bitween Dsleft and dsright points
+                DsPixel = np.sqrt( (dsRight[0] - dsLeft[0])**2 + (dsRight[1] - dsLeft[1])**2 )
+                DsMetric=DsPixel/ratio
         
-        # dsTemp is the coornidates of countour that is same as deTop
-        dsTemp=c[np.where(c[:,:,:]==deTop[1]), 0 ][0]
+            # put circles and coordinates of deLeft and deRight and deTop and deDown in screen
+            cv2.circle(frame, deLeft, 5, (0, 0, 255), -1)
+            cv2.circle(frame, deRight, 5, (0, 0, 255), -1)
+            cv2.circle(frame, deTop, 5, (0, 0, 255), -1)
+            cv2.circle(frame, deDown, 5, (0, 0, 255), -1)
     
-        # if dsTemp has coornidates then grab most left and most right and put it to dsLeft,dsRight
-        if dsTemp.size:
-            dsLeft=(np.amax(dsTemp[:,0]),deTop[1])
-            dsRight=(np.amin(dsTemp[:,0]),deTop[1])
-            # put circles and coordinates of dsLeft and dsRight in screen
-            cv2.circle(frame, dsLeft, 5, (0, 255, 12), -1)
-            cv2.circle(frame, dsRight, 5, (0, 255, 12), -1)
-            #calculate distance bitween Dsleft and dsright points
-            DsPixel = np.sqrt( (dsRight[0] - dsLeft[0])**2 + (dsRight[1] - dsLeft[1])**2 )
-            DsMetric=DsPixel/ratio
+
+            #calculate distance bitween Deleft and deright points
+            DePixel = np.sqrt( (deRight[0] - deLeft[0])**2 + (deRight[1] - deLeft[1])**2 )
+            DeMetric=DePixel/ratio
+
+            #calculate S
+            S=DsMetric/DeMetric
+            H=0
+            if S>0:
+                #calculat H
+                H=0.3168*S**(-2.612)
         
-        # put circles and coordinates of deLeft and deRight and deTop and deDown in screen
-        cv2.circle(frame, deLeft, 5, (0, 0, 255), -1)
-        cv2.circle(frame, deRight, 5, (0, 0, 255), -1)
-        cv2.circle(frame, deTop, 5, (0, 0, 255), -1)
-        cv2.circle(frame, deDown, 5, (0, 0, 255), -1)
-    
 
-        #calculate distance bitween Deleft and deright points
-        DePixel = np.sqrt( (deRight[0] - deLeft[0])**2 + (deRight[1] - deLeft[1])**2 )
-        DeMetric=DePixel/ratio
+            #calculate IFT 
+            delta=float(self.bulkVar.get())-float(self.dropVar.get())
+            IFT=0
+            if H>0:
+                IFT=0.01*((delta*9.8*(DeMetric**2))/H)
 
-        #calculate S
-        S=DsMetric/DeMetric
-
-        #calculat H
-        H=0.3168*S**(-2.612)
-
-
-        #calculate IFT 
-        delta=float(int(self.bulkVar.get()))-float(int(self.dropVar.get()))
-        IFT=0.01*((delta*9.8*(DeMetric**2))/H)
-
-        # print de and ds and S values in image
-        if S>0:
-            cv2.rectangle(frame,(10,420),(630,470),(10,160,52),-1)
-            cv2.putText(frame,"Time="+str(datetime.datetime.now().time().strftime('%H:%M:%S')),(10,440), font, 1.5, (150,20,100),1, cv2.LINE_AA)
-            cv2.putText(frame,"IFT="+str(IFT),(10,460), font, 1.5, (200,0,0),1, cv2.LINE_AA)
-            self.tree.insert("" , 0, values=(str(datetime.datetime.now().time().strftime('%M:%S')),str(IFT)))
-            self.csvList.append([str(datetime.datetime.now().time().strftime('%H:%M:%S')),str(IFT)])
+            # print de and ds and S values in image
+            if S>0 and H>0 and IFT>0:
+                cv2.rectangle(frame,(10,420),(630,470),(10,160,52),-1)
+                cv2.putText(frame,"Time="+str(datetime.datetime.now().time().strftime('%H:%M:%S')),(10,440), font, 1.5, (150,20,100),1, cv2.LINE_AA)
+                cv2.putText(frame,"IFT="+str(IFT),(10,460), font, 1.5, (200,0,0),1, cv2.LINE_AA)
+                self.tree.insert("" , 0, values=(str(datetime.datetime.now().time().strftime('%M:%S')),str(IFT)))
+                self.csvList.append([str(datetime.datetime.now().time().strftime('%H:%M:%S')),str(IFT)])
+        
+        else:
+            print('i cant detect object pls customize and define setting')
         if ret:
             self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
             self.canvas.create_image(0, 0, image = self.photo, anchor = tk.NW)
@@ -302,8 +329,33 @@ class App:
         #clear the config object
         ConfigObj.clear(config)
 
-#get video streaming path and set the frame width and height 
+    def updateShow(self):
+        #read configuration file and put it to config array
+        config = ConfigObj('conf.cnf')
+        self.delay = config["interval"]
+        # Get a frame from the video source
+        vid=cv2.VideoCapture(int(config["path"]))
+        ret, frame = vid.read()
+        frame = cv2.resize(frame, (530, 397)) 
+        
+        #read frame from video and convert to gray then thresh then find edge
+        if config["Ds"]=="top":
+            frame = cv2.flip(frame, 1)
+        elif  config["Ds"]=="down":
+            frame = cv2.flip(frame, 0) 
+        if ret:
+            self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image = self.photo, anchor = tk.NW)
+            self.canvas.bind("<Button-1>",self.DoCalibrate)
+        
+        if self.show_switch==True:
+            self.showButton["text"]="StopStream" 
+            self.root.after(self.delay, self.updateShow)
+        #clear the config object
+        ConfigObj.clear(config)
+
 class Do_Analysis:
+#get video streaming path and set the frame width and height class Do_Analysis:
     def __init__(self, video_source=0):
         
         #read configuration file and put it to config array
@@ -319,7 +371,9 @@ class Do_Analysis:
     def get_frame(self):
         if self.vid.isOpened():
             #read configuration file
-            ret, frame = self.vid.read()
+            vid = cv2.VideoCapture(int(config["path"]))
+            
+            ret, frame = vid.read()
             frame = cv2.resize(frame, (530, 397)) 
             if ret:
                 # Return a boolean success flag and the current frame converted to BGR
@@ -334,7 +388,6 @@ class Do_Analysis:
     def __del__(self):
         if self.vid.isOpened():
             self.vid.release()
-
 
 # Create a window and pass it to the Application object
 App(tk.Tk(), "DropVision Main")
